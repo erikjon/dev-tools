ff6b8e778065824d79d57ea3f6f5a38fe4f7cad2
Erik HANSON <erik.j.hanson@navitaire.com>
2018-06-04 09:51:15 -0500

    Intial commit - LoremIpsum and PseudoRandomNumbers

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace devutils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LoremIpsumGenerator.Get(10));
+        }
+    }
+}
---
+++ src/PseudoRandomNumberGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace devutils
+{
+    public class PseudoRandomNumberGenerator
+    {
+        static Random random;
+
+        static PseudoRandomNumberGenerator()
+        {
+            random = new Random((int)DateTime.UtcNow.Ticks / 2048);
+        }
+
+        public static int Next()
+        {
+            return random.Next();
+        }
+
+        public static int Next(int maxValue)
+        {
+            return random.Next(maxValue);
+        }
+
+        public static int Next(int minValue, int maxValue)
+        {
+            return random.Next(minValue, maxValue);
+        }
+    }
+}
